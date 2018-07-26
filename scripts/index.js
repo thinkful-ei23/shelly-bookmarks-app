7c113ea2213bfb9cb5c836f7c8ed3fb8a98c4358
Shelly Leung <pontificable@yahoo.com>
2018-07-26 13:31:44 -0700

    working form + serialization

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
+/* global $ */
+
 function handleExpand() {
 	//when the condensed block is clicked, toggle condensed property
-	$().
 }
 
 function handleGenerateBookmark() {
 	//when submit pushed, make a new bookmark
-	$().
+	$('.js-bookmarks-add-form').on('submit', e => {
+		e.preventDefault();
+		console.log('submit', e);
+		console.log(serializeJson(e.target));
+	});
 }
+
+function serializeJson(form) {
+	const formData = new FormData(form);
+	const obj = {};
+	formData.forEach((val, name) => obj[name] = val);
+	return JSON.stringify(obj);
+
+}
+
+$(handleGenerateBookmark);
